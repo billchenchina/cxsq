6288b8602f89a6601a832115bf78fe205ad241bd
billchenchina <billchenchina2001@gmail.com>
2020-12-19 10:43:04 +0800

    Refactor
    Co-authored-by: leylee <leylee2001@gmail.com>

--- cxsq/cxsq.py
+++ cxsq/cxsq.py
@@ -1,34 +1,33 @@
-import hit
-from requests import Session
-import logging
-import yaml
-import json
 import argparse
 import datetime
+import json
+import logging
 import traceback
-
-
 from typing import Tuple
+
+import hit
+import requests
+import yaml
+from requests import Session
 
 logging.basicConfig(level=logging.INFO)
 
 
 def read_config(filename: str) -> Tuple[str, str]:
     try:
-        logging.info("Reading config from %s" % filename)
+        logging.info("Reading config from %s", filename)
         o = open(filename, 'r')
         c = yaml.load(o, Loader=yaml.SafeLoader)
         ret = (c['username'], c['password'])
         return ret
     except OSError:
-        logging.error('Fail to read configuration from %s' % filename)
+        logging.error('Fail to read configuration from %s', filename)
     except yaml.YAMLError:
         logging.error('Fail to parse YAML')
     exit(1)
 
 
 def get_xg_session(username: str, password: str) -> Session:
-    # TODO
     # hit.ids.login()
     logging.info('Logging in to xg.hit.edu.cn')
     try:
@@ -61,7 +60,7 @@ def main():
                             const=arg_today, help='add today to date list')
     date_group.add_argument('-T', '--tomorrow', action='append_const', dest='dates',
                             const=arg_tomorrow, help='add tomorrow to date list')
-    date_group.add_argument('-d', '--dates', nargs='+', action='extend', dest='dates',
+    date_group.add_argument('-d', '--dates', nargs='+', action='append', dest='dates',
                             metavar='date', help='yyyyMMdd, add several dates to date list')
 
     args = parser.parse_args()
@@ -73,36 +72,44 @@ def main():
     if not args.dates:
         parser.error('No dates given, add -t, -T or -d')
     try:
+        dates = []
+        for i in args.dates:
+            if isinstance(i, list):
+                for _ in i:
+                    dates.append(_)
+            else:
+                dates.append(i)
         datelist = [datetime.datetime.strptime(
-            arg, arg_date_format).date() for arg in args.dates]
-        logging.info('datelist: ' + str(datelist))
-    except Exception as e:
+            arg, arg_date_format).date() for arg in dates]
+        logging.info('datelist: %s', datelist)
+    except ValueError as e:
         parser.error('wrong date format, format is yyyyMMdd')
     username, password = read_config(args.conf_file)
     session = get_xg_session(username, password)
     date: datetime.date
     for date in datelist:
         try:
-            logging.info('cxsq on ' + date.isoformat())
+            logging.info('cxsq on %s', date.isoformat())
             cxsq(session, date)
         except Exception as e:
-            logging.error('error: ' + str(e))
+            logging.error('error: %s', e)
 
 
 def cxsq(session: Session, date: datetime.date):
     try:
         response = session.post(
-            'https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsCxsq/getCxsq', data={'info': '{"id": "id"}'})
+            'https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsCxsq/getCxsq',
+            data={'info': '{"id": "id"}'})
         if not response.ok:
-            logging.error('response.status_code:' + str(response.status_code))
+            logging.error('response.status_code: %s', response.status_code)
             raise Exception('Failed to get getCxsq')
-        logging.debug('response.status_code:' + str(response.status_code))
-        logging.debug('response.text:' + response.text)
+        logging.debug('response.status_code: %s', response.status_code)
+        logging.debug('response.text: %s', response.text)
         cxsq = response.json()
         if not cxsq['isSuccess']:
-            logging.error('response: ' + str(cxsq))
+            logging.error('response: %s', cxsq)
             raise Exception('Failed to get getCxsq')
-        id = cxsq['module']['id']
+        _id = cxsq['module']['id']
         # d.strftime('%m-%-d')
         # d.strftime('%-m-%-d')
         # '12-7'
@@ -113,7 +120,7 @@ def cxsq(session: Session, date: datetime.date):
                 'cxly': ' ',
                 'cxlx': '01',
                 'yjlxjsrq': '',
-                'id': id,
+                'id': _id,
             }
         }
         body = {
@@ -128,6 +135,8 @@ def cxsq(session: Session, date: datetime.date):
         cxsq = response.json()
         if not cxsq['isSuccess']:
             raise Exception(cxsq['msg'])
+    except requests.RequestException as e:
+        raise e
     except Exception as e:
         raise e
 
